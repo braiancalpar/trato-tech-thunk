4a711858e6c4378a4a197d3008a6563c1ac6d66e
braiancalpar <braian.santos@calpar.com.br>
2026-07-31 11:08:25 -0300

    feat: adicionar funcionalidade para buscar e adicionar itens ao estado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,7 @@ import Button from "components/Button";
 import { useCallback, useEffect } from "react";
 import instance from "common/config/api";
 import { adicionarCategorias } from "store/reducers/categorias";
+import { adicionarItens } from "store/reducers/itens";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -14,14 +15,21 @@ export default function Home() {
   const categorias = useSelector((state) => state.categorias);
 
   const buscarCategorias = useCallback(async () => {
-    const resposta = await instance.get('/categorias')
+    const resposta = await instance.get("/categorias");
 
-    dispatch(adicionarCategorias(resposta.data))
-  }, [dispatch])
+    dispatch(adicionarCategorias(resposta.data));
+  }, [dispatch]);
+
+  const buscarItens = useCallback(async () => {
+    const resposta = await instance.get("/itens");
+
+    dispatch(adicionarItens(resposta.data));
+  }, [dispatch]);
 
   useEffect(() => {
-    buscarCategorias()
-  }, [buscarCategorias])
+    buscarCategorias();
+    buscarItens();
+  }, [buscarCategorias, buscarItens]);
 
   return (
     <div>
